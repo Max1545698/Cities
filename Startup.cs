d6d309cd05d018de4535fba87254148b3e5e7e4a
Максим Рожок <max15071999@gmail.com>
2018-08-31 18:07:43 +0300

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cities.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cites
+{
+    public class Startup
+    {
+        private IConfiguration configuration;
+
+        public Startup(IConfiguration config)
+        {
+            configuration = config;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IRepository, MemoryRepository>();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseBrowserLink();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
